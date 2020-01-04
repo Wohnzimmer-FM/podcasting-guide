f75d1b7ff1e60febb854011087ddee90c0696737
leonidlezner <kontakt@lezner.de>
2020-01-04 21:10:01 +0100

    Generating the docbook xml

--- build.sh
+++ build.sh
@@ -2,10 +2,10 @@ mkdir -p dist
 
 asciidoctor ./src/index.adoc -D dist
 
-#asciidoctor -b docbook ./src/index.adoc -D dist
+asciidoctor -b docbook ./src/index.adoc -D dist
 
 #pandoc --from docbook --to pdf -s ./dist/index.xml -o dist/index.pdf
 
 #pandoc --from docbook --to epub -s ./dist/index.xml -o dist/index.epub
 
-asciidoctor-pdf ./src/index.adoc -o dist/index.pdf
+#asciidoctor-pdf ./src/index.adoc -o dist/index.pdf
